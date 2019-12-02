3ece60be099ae4fa723145ed5f5c302f42d71e05
Mitch Lloyd <mitch@northcoders.com>
2019-12-02 09:42:07 +0000

    remove all the detritus from this repo

--- katas/are-ordered.js
+++
@@ -1,9 +0,0 @@
-/*
-  the areOrdered function should take an array of numbers as an input.
-  It should return true if all the numbers are in ascending order and false if they are not.
-  An empty array should return false.
-*/
-
-function areOrdered() {}
-
-module.exports = areOrdered;
