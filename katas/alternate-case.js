8b9b9fdbaff4f991637792d6b6a38ef7d030a60a
technicole-t <thorleynicole@gmail.com>
2020-02-08 17:26:41 +0000

    alternate case kata complete

--- katas/alternate-case.js
+++ katas/alternate-case.js
@@ -6,6 +6,16 @@
   'hello world' would become 'HeLlO wOrLd'
 */
 
-function alternateCase() {}
+function alternateCase(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (i % 2 === 0) {
+      result += str.charAt(i).toUpperCase();
+    } else {
+      result += str.charAt(i).toLowerCase();
+    }
+  }
+  return result;
+}
 
 module.exports = alternateCase;
--- spec/alternate-case.spec.js
+++ spec/alternate-case.spec.js
@@ -1,7 +1,23 @@
-const alternateCase = require('../katas/alternate-case');
+const alternateCase = require("../katas/alternate-case");
+const { expect } = require("chai");
 
-describe('alternateCase', () => {
-  it('describe what happens if this test succeeds here', () => {
-    // expect(alternateCase(???)).to...
+describe.only("alternateCase", () => {
+  it("returns a string of two letters with the first letter capitalised and the second letter lowercase", () => {
+    const str = "hi";
+    const actual = alternateCase(str);
+    const expected = "Hi";
+    expect(actual).to.eql(expected);
+  });
+  it("returns a string of one long word with every other letter capitalised", () => {
+    const str = "concentrate";
+    const actual = alternateCase(str);
+    const expected = "CoNcEnTrAtE";
+    expect(actual).to.eql(expected);
+  });
+  it("returns a string with more than one work with every other letter capitalised", () => {
+    const str = "once upon a time";
+    const actual = alternateCase(str);
+    const expected = "OnCe uPoN A TiMe";
+    expect(actual).to.eql(expected);
   });
 });
