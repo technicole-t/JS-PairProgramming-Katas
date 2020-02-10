b97315f8ec5b4a3be58af046979a964942ba6f4e
technicole-t <thorleynicole@gmail.com>
2020-02-10 16:28:35 +0000

    solved sum-digits problem using TDD

--- katas/sum-digits.js
+++ katas/sum-digits.js
@@ -7,6 +7,32 @@
     --> 6
 */
 
-function sumDigits() {}
+function sumDigits(nums) {
+  if (nums === undefined) return 0;
+  const stringNum = nums
+    .toString()
+    .replace(/\./, "")
+    .split("");
+  const changeToNumber = stringNum.map(num => Number(num));
+  return changeToNumber.reduce((a, b) => a + b);
+}
+// let turnToString = nums.toString();
+// let regex = /\d+/g;
+// let onlyNums = turnToString.match(regex);
+// console.log(onlyNums);
+// let singleDigits = onlyNums.map(num => num.split(""));
+// let stringToNum = singleDigits.map(item => Number(item));
+// console.log(stringToNum);
+// let singleDigits = onlyNums.split("");
+// console.log(singleDigits);
+// console.log(turnToString);
+// let allNumbers = turnToString.filter(num => typeof num === "number");
+// console.log(allNumbers);
+// let total = 0;
+// for (let i = 0; i < turnToString.length; i++) {
+//   if (turnToString.includes("."))
+//   total += Number(turnToString[i]);
+// }
+// return total;
 
 module.exports = sumDigits;
--- spec/sum-digits.spec.js
+++ spec/sum-digits.spec.js
@@ -1,7 +1,25 @@
-const sumDigits = require('../katas/sum-digits');
+const sumDigits = require("../katas/sum-digits");
+const { expect } = require("chai");
 
-describe('sumDigits', () => {
-  it('describe what happens if this test succeeds here', () => {
-    // expect(sumDigits()).to...
+describe.only("sumDigits", () => {
+  it("returns 0 when no argument is passed", () => {
+    const sum = sumDigits();
+    const expected = 0;
+    expect(sum).to.eql(expected);
+  });
+  it("returns the number passed when only one number is passed", () => {
+    const sum = sumDigits(5);
+    const expected = 5;
+    expect(sum).to.eql(expected);
+  });
+  it("returns the sum of the two digits when a two-digit number is passed", () => {
+    const sum = sumDigits(22);
+    const expected = 4;
+    expect(sum).to.eql(expected);
+  });
+  it("returns the sum of a number passed that is not an integer", () => {
+    const sum = sumDigits(22.2);
+    const expected = 6;
+    expect(sum).to.eql(expected);
   });
 });
