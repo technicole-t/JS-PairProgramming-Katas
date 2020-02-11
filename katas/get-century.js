811989831f414d4a835513c62cf2f06204592c75
Rosalyn Land <roz.s.land@gmail.com>
2020-02-11 12:58:39 +0000

    solved getCentury problem using TDD

--- katas/get-century.js
+++ katas/get-century.js
@@ -6,6 +6,23 @@
   It should work up to and including the year 10,000 (the '101st' century)
 */
 
-function getCentury() {}
+function getCentury(year) {
+  if (year === undefined) return "Please enter a year";
+
+  const yearStart = Math.floor(year / 100) + 1;
+  const century = yearStart.toString();
+  const thRegex = /[0|4-9]/g;
+  const endCharCentury = century[century.length - 1];
+
+  if ((thRegex.test(endCharCentury) === true) || yearStart > 10 && yearStart < 20) {
+    return `${century}th`;
+  } else if (endCharCentury === "1") {
+    return `${century}st`
+  } else if (endCharCentury === "2") {
+    return `${century}nd`
+  } else if (endCharCentury === "3") {
+    return `${century}rd`
+  }
+};
 
 module.exports = getCentury;
--- spec/get-century.spec.js
+++ spec/get-century.spec.js
@@ -1,7 +1,44 @@
 const getCentury = require('../katas/get-century');
+const { expect } = require('chai');
 
-describe('getCentury', () => {
-  it('describe what happens if this test succeeds here', () => {
-    // expect(getCentury(???)).to...
+describe.only('getCentury', () => {
+  it("returns 'Please enter a year' if no argument is passed", () => {
+    expect(getCentury()).to.equal("Please enter a year");
+  });
+  it("returns a string containing the correct century if passed a 'th' year", () => {
+    let actual = getCentury(1999)
+    let expected = "20th"
+    expect(actual).to.equal(expected)
+
+    actual = getCentury(1400)
+    expected = "15th"
+    expect(actual).to.equal(expected)
+  });
+  it("returns a string containing the correct century if passed a 'st' year", () => {
+    let actual = getCentury(2000)
+    let expected = "21st"
+    expect(actual).to.equal(expected)
+
+    actual = getCentury(10000);
+    expected = '101st';
+    expect(actual).to.equal(expected)
+  });
+  it("returns a string containing the correct century if passed a 'nd' year", () => {
+    let actual = getCentury(2122)
+    let expected = "22nd"
+    expect(actual).to.equal(expected)
+
+    actual = getCentury(9100);
+    expected = '92nd';
+    expect(actual).to.equal(expected)
+  });
+  it("returns a string containing the correct century if passed a 'rd' year", () => {
+    let actual = getCentury(2222)
+    let expected = "23rd"
+    expect(actual).to.equal(expected)
+
+    actual = getCentury(9200);
+    expected = '93rd';
+    expect(actual).to.equal(expected)
   });
 });
