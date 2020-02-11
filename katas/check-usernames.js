d713f20cdceb4428c6eba5cf35bdfb3e632f4462
Rosalyn Land <roz.s.land@gmail.com>
2020-02-11 16:52:48 +0000

    solved for an array with one username

--- katas/check-usernames.js
+++ katas/check-usernames.js
@@ -3,14 +3,17 @@
   return true if they are all valid and false if any are not valid.
   A valid username:
   - is at least 5 characters long
-  - may contain lowercase letters, numbers and underscores
+  - may only contain lowercase letters, numbers and underscores
   - is no longer than 20 characters
 */
 
 function checkUsernames(usernames) {
   if (usernames.length === 0) return false;
-  if (usernames[0].length >= 5 && usernames[0].length <= 20) return true;
+  const usernameRulezRegex = /^[a-z0-9_]{5,20}$/g;
+  if (usernameRulezRegex.test(usernames[0])) {
+    return true;
+  };
   return false;
-}
+};
 
 module.exports = checkUsernames;
--- spec/check-usernames.spec.js
+++ spec/check-usernames.spec.js
@@ -8,24 +8,46 @@ describe.only("checkUsernames", () => {
     const expected = false;
     expect(actual).to.equal(expected);
   });
-  it("returns true if there is only one username in the array which is at least 5 characters", () => {
+  it("returns true if the one username passed is at least 5 characters", () => {
     let usernames = ["dumbledore"];
     let actual = checkUsernames(usernames);
     let expected = true;
     expect(actual).to.equal(expected);
-
-    usernames = ["roy"];
-    actual = checkUsernames(usernames);
-    expected = false;
-    expect(actual).to.equal(expected);
   });
-  it("returns false if there is only one username in the array which is more than 20 characters", () => {
-    let usernames = ["supercalifragilisticexpialidocious"];
+  it("returns false if the one username passed is less than 5 characters", () => {
+    let usernames = ["roy"];
     let actual = checkUsernames(usernames);
     let expected = false;
     expect(actual).to.equal(expected);
   });
-  // it("may only contain lowercase letters, numbers and underscores", () => {
-
-  // })
+  it("returns false if there is only one username in the array which is more than 20 characters", () => {
+    const usernames = ["supercalifragilisticexpialidocious"];
+    const actual = checkUsernames(usernames);
+    const expected = false;
+    expect(actual).to.equal(expected);
+  });
+  it("returns false if the username contains uppercase characters", () => {
+    const usernames = ["Dumbledore"];
+    const actual = checkUsernames(usernames);
+    const expected = false;
+    expect(actual).to.equal(expected);
+  });
+  it("returns true if the username contains letters and numbers", () => {
+    const usernames = ["dumbl3d0r3"];
+    const actual = checkUsernames(usernames);
+    const expected = true;
+    expect(actual).to.equal(expected);
+  });
+  it("returns true if the username contains only numbers", () => {
+    const usernames = ["345678"];
+    const actual = checkUsernames(usernames);
+    const expected = true;
+    expect(actual).to.equal(expected);
+  });
+  it("returns true if the username contains an underscore", () => {
+    const usernames = ["booooo_m"];
+    const actual = checkUsernames(usernames);
+    const expected = true;
+    expect(actual).to.equal(expected);
+  });
 });
