9c8c5774542302ae8433538569a9ab350eea0f40
technicole-t <thorleynicole@gmail.com>
2020-02-11 14:55:27 +0000

    check user name part complete, 2 tests passing

--- spec/check-usernames.spec.js
+++ spec/check-usernames.spec.js
@@ -1,7 +1,22 @@
-const checkUsernames = require('../katas/check-usernames');
+const checkUsernames = require("../katas/check-usernames");
+const { expect } = require("chai");
 
-describe('checkUsernames', () => {
-  it('describe what happens if this test succeeds here', () => {
-    // expect(checkUsernames()).to...
+describe.only("checkUsernames", () => {
+  it("returns true if the username is at least 5 characters", () => {
+    let username = "dumbledore";
+    let actual = checkUsernames(username);
+    let expected = true;
+    expect(actual).to.equal(expected);
+
+    username = "roy";
+    actual = checkUsernames(username);
+    expected = false;
+    expect(actual).to.equal(expected);
+  });
+  it("returns username is no longer than 20 characters", () => {
+    let username = "supercalifragilisticexpialidocious";
+    let actual = checkUsernames(username);
+    let expected = false;
+    expect(actual).to.equal(expected);
   });
 });
